9a5564eec80e19545dead94127a042d17e9da3f7
fujiya228 <fujiya0xE2@gmail.com>
2022-06-12 14:54:06 +0900

    ベースとなる画面を作成

---
+++ lib/views/root.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_memo_sample/views/home_page.dart';
+
+class Root extends StatefulWidget {
+  const Root({Key? key}) : super(key: key);
+
+  @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  int currentIndex = 0;
+  List<StatelessWidget> pageWidgetList = [
+    const Home(),
+    const Home(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: IndexedStack(
+          index: currentIndex,
+          children: pageWidgetList,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          child: const Icon(Icons.edit),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) => {
+            setState(
+              () => currentIndex = index,
+            )
+          },
+          currentIndex: currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'home',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
